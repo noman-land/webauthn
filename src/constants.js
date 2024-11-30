e9599588826b9be64a89f0764340d47ba22050d7
Noman <noman@noman.land>
2024-11-29 22:01:22 -0500

    Modernize and make ts, hono, update deps

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-// The app ID is a string used to uniquely identify your U2F app, for both registration requests and
-// authentication requests. It is usually the fully qualified URL of your website. The website MUST
-// be HTTPS, otherwise the registration will fail client-side.
-export const APP_ID = 'noman.land';
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,11 @@
-export const randomBytes = n =>
+export const randomBytes = (n: number) =>
   [...Array(n)]
     .map(() => Math.floor(Math.random() * Math.pow(2, 8)).toString(16))
     .join('');
 
 export class JsonResponse extends Response {
   constructor(body, ...rest) {
+    super(body, ...rest)
     return new Response(JSON.stringify(body), {
       ...rest,
       headers: {
